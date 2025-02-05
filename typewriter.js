db77bf7760c79c0ceba1df4a08b057dc1b344d7f
dogmaNN <agaghihi7gg@gmail.com>
2025-02-05 18:17:33 +0800

    change the padding of .helloWorld, and the margin of menu. also add typewriter(but its too difficult, so i decide make it in future.

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,20 @@
 const typewriterH1 = document.querySelector(".typewriter h1");
 let text = ["Hello there", "你好", "Make videos, Develop", "also play some roblox games"]
 let t = 0;
+let i = 0;
 
 function typewriter(){
     while (true){
+        if (i > text.length) {
+            i = 0
+        }
         if (t < text[i].length) {
             typewriterH1.innerHTML += text[0].charAt(t);
             t++;
             setTimeout(typewriter, 100);
         }
+
+        i++;
     }
 }
 
